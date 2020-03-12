663697a2d63ce67a1637faa395eabe7b133f6f8e
Ornela Piermattei <orne.leones@gmail.com>
2020-03-12 17:02:14 -0300

    Primera version

---
+++ index.php
@@ -0,0 +1,117 @@
+<!doctype html>
+<html lang="es">
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="description" content="Portfolio">
+      <meta name="author" content="">
+      <link rel="icon" type="image/png" href="img/icono.png">
+
+      <title>Portfolio - OP</title>
+
+      <link href="css/bootstrap.min.css" rel="stylesheet">
+      <link href="css/style.css" rel="stylesheet">
+
+      <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700" rel="stylesheet">
+   
+   </head>
+   <body>
+
+      <section class="datos-section">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-1 col-lg-2"></div>
+               <div class="col-md-10 col-lg-8">
+                  <div class="datos">
+                     <div class="img-perfil"><img src="images/perfil.jpg" alt=""></div>
+                     <h2><b>Ornela Piermattei</b></h2>
+                     <h4 class="font-yellow">Ing. en Sistemas de Información</h4>
+                     <ul class="mt-4 mb-4 pl-0">
+                        <li><b>NACIMIENTO : </b>3 de Abril de 1987</li>
+                        <li><b>EMAIL : </b>ornelapiermattei@hotmail.com</li>
+                     </ul>
+                     <ul class="mt-4 mb-0 pl-0">
+                        <li>Programación y administración de base e datos. Creación de reportes del sistema segun necesidades del cliente.
+                        </li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+
+      <section class="experiencia-section mt-5">
+         <div class="container">
+            <div class="row justify-content-around">
+               <div class="col-lg-12 text-center">
+                  <h2 class="section-title">Experiencia</h2>
+               </div>
+               <div class="col-lg-3 col-md-4 text-center">
+                  <img src="images/experiencia/icon-1.png" alt="icon">
+                  <p class="mb-0">Julio 2011 - Actualidad</p>
+                  <h4>Desarrollo sistemas Web</h4>
+                  <h6 class="text-secondary">Senior</h6>
+               </div>
+               <div class="col-lg-3 col-md-4 text-center">
+                  <img src="images/experiencia/icon-2.png" alt="icon">
+                  <p class="mb-0">Noviembre 2019 - Actualidad</p>
+                  <h4>UX Experiencia de Usuario</h4>
+                  <h6 class="text-secondary">Training</h6>
+               </div>
+               <div class="col-lg-3 col-md-4 text-center">
+                  <img src="images/experiencia/icon-3.png" alt="icon">
+                  <p class="mb-0">Sep 2014 - Present</p>
+                  <h4>Desarrollo de Páginas Web</h4>
+                  <h6 class="text-secondary">Junior</h6>
+               </div>
+            </div>
+         </div>
+      </section>
+
+
+
+
+
+
+
+
+
+      <footer class="bg-dark footer-section">
+         <div class="section">
+            <div class="container">
+               <div class="row">
+               <div class="col-md-4">
+                  <h5 class="text-light">F</h5>
+               </div>
+               <div class="col-md-4">
+                  <a href="#"><i class="ion-social-instagram"></i></a>
+               </div>
+               <div class="col-md-4">
+                  <h5 class="text-light">In</h5>
+               </div>
+               </div>
+            </div>
+         </div>
+         <div class="border-top text-center border-secondary py-5">
+            <p class="mb-0 text-secondary">Copyright ©
+               <script>
+                  var CurrentYear = new Date().getFullYear()
+                  document.write(CurrentYear)
+               </script>
+            </p>
+         </div>
+      </footer>
+      
+
+
+
+
+
+
+      <!-- Bootstrap core JavaScript
+      ================================================== -->
+      <!-- Placed at the end of the document so the pages load faster -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <script src="js/bootstrap.min.js"></script>      
+   </body>
+</html>
